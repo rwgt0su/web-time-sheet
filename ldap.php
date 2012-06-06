b797b89a23d5559cffaa3a26b61dc002bace7292
danlumpp <danlumpp@gmail.com>
2012-06-06 17:25:39 +0000

    bad filename

--- ldap.php
+++ ldap.php
@@ -6,35 +6,38 @@ $ldaprdn  = $_POST['user'];     // ldap rdn or dn
 $ldappass = $_POST['pass'];  // associated password
 
 if (isset($_POST['Submit']))
-    echo "Submit button pressed!";
+    echo "Submit button pressed!</br>";
 
-if (!isset($ldaprdn) || !isset($ldappass))
-{
+//echo '$ldaprdn is ' . $ldaprdn . '. </br>';
+echo '$_POST[\'user\'] is ' . $_POST['user'] . '. </br>';
+
+if (empty($ldaprdn) || empty($ldappass)){
   echo "One or more fields blank. Bound anonymously</br>";
 }
+else{
+    //append domain to user name
+    $ldaprdn = $ldaprdn . '@sheriff.mahoning.local';
 
-//append domain to user name
-$ldaprdn = $ldaprdn . '@sheriff.mahoning.local';
+    // connect to ldap server
+    $ldapconn = ldap_connect("10.1.35.110")
+        or die("Could not connect to LDAP server.");
 
-// connect to ldap server
-$ldapconn = ldap_connect("10.1.35.110")
-    or die("Could not connect to LDAP server.");
+      echo $ldaprdn . "</br>";
 
-  echo $ldaprdn . "</br>";
+    if ($ldapconn)
+      {
+        // binding to ldap server
+        $ldapbind = ldap_bind($ldapconn, $ldaprdn, $ldappass);
+        //$ldapbind = ldap_bind($ldapconn); //anonymous
 
-if ($ldapconn)
-  {
-    // binding to ldap server
-    $ldapbind = ldap_bind($ldapconn, $ldaprdn, $ldappass);
-    //$ldapbind = ldap_bind($ldapconn); //anonymous
+        // verify binding
+        if ($ldapbind) {
+            echo "LDAP bind successful...";
+        } else {
+            echo "LDAP bind failed...";
+        }
 
-    // verify binding
-    if ($ldapbind) {
-        echo "LDAP bind successful...";
-    } else {
-        echo "LDAP bind failed...";
-    }
-
-  }
+      }
+}
 
 ?>
