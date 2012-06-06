61c9e1d522e0f3a6f57a11351c763034d1e6ac5a
danlumpp@gmail.com <danlumpp@gmail.com@ad210744-6c23-267b-c354-416c3078f2c7>
2012-06-06 17:25:39 +0000

    bad filename

--- trunk/emp_query_form.php
+++ trunk/emp_query_form.php
@@ -7,7 +7,7 @@
 ?>
 
 <html><body>
-<form name="emp" method="post" action="emp_query.php">
+<form name="emp" method="post" action="emp_query_sql.php">
 	<h1>Employee lookup</h1>
         
 	<p>Enter an employee ID:<input type="text" name="ID"></p>
