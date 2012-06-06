ee361c696b86ba900cebb085ea3ebb655b53c92e
danlumpp <danlumpp@ad210744-6c23-267b-c354-416c3078f2c7>
2012-06-06 17:12:19 +0000

    1st upload

---
+++ trunk/emp_query_sql.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$ID=$_POST['ID'];
+
+$myq = "SELECT * FROM EMPLOYEE WHERE ID=$ID";
+$result = $mysqli->query($myq);
+?>
+
+Contents of employee table</br>
+<table border="1">
+<tr>
+<?php
+$numOfCols = mysql_num_fields($result);
+//$numOfRows = mysql_num_rows($result);
+
+for($i=0; $numOfCols > $i+1; $i++)
+{
+echo "<th>" . mysql_field_name($result, $i) . "</th>";
+}
+?>
+    
+</tr>
+
+<tr>
+<?php
+
+$fieldCounter=0;
+
+$result->data_seek(0);  //moves internal pointer to 0, fetch starts here
+while ($row = $result->fetch_assoc()) //fetch assoc array && pointer++
+   {
+    $fieldName=mysql_field_name($result, $fieldCounter);
+	 echo "<td>${row["$fieldName"]}</td>";
+         $fieldCounter++;
+   }
+?>
+</tr>
+</table>
