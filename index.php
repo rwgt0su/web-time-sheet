7c0f72062b013fca303697e7a5b776e9835a85b0
danlumpp <danlumpp@ad210744-6c23-267b-c354-416c3078f2c7>
2012-06-06 17:12:58 +0000

    1st upload

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
-<h1>Payroll Project Testing</h1>
-
-<a href="db_test.php">DB Query Test</a>
-</br>
-<a href="login.php">Login Test</a>
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
