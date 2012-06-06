aa1199b04611cea34b76a28618727c4c7a5d0581
danlumpp <danlumpp@ad210744-6c23-267b-c354-416c3078f2c7>
2012-06-06 17:20:31 +0000

    was mangled inexplicably

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
-<!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
+    
     <body>
-        <?php
-        // put your code here
-        ?>
+        <h1>Payroll Project Testing</h1>
+
+<a href="db_test.php">DB Query Test</a>
+</br>
+<a href="login.php">Login Test</a>
+</br>
+<a href="insert_user.php">Insert New User</a>
+</br>
+<a href="emp_query_form.php">Employee Lookup</a>
+     
     </body>
 </html>
