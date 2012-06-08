69560bbcea46bd4086b5e49644b9de39a8990f30
danlumpp <danlumpp@ad210744-6c23-267b-c354-416c3078f2c7>
2012-06-08 15:16:49 +0000

    change DB to PAYROLL

--- trunk/bin/db_sqli.php
+++ trunk/bin/db_sqli.php
@@ -1,7 +1,7 @@
 <?php
 function connectToSQL(){
     //establish connetcion to DB
-    $mysqli = new mysqli("localhost", "web", "10paper", "test");
+    $mysqli = new mysqli("localhost", "web", "10paper", "PAYROLL");
     if ($mysqli->connect_errno) {
         $error = "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
         echo $error;
