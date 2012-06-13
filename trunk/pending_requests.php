85f58b6a3eb76183e272fba68785bdfd3944dd94
danlumpp <danlumpp@ad210744-6c23-267b-c354-416c3078f2c7>
2012-06-13 17:58:49 +0000

    IP

--- trunk/pending_requests.php
+++ trunk/pending_requests.php
@@ -6,22 +6,47 @@
  */
 
 require_once 'bin/common.php';
+popUpMessage("HELLO WORLD!");
+$admin = $_SESSION['admin'];
 
-$mysqli = connectToSQL();
-$myq = "SELECT REFER 'Ref. No.', REQDATE 'Requested', USEDATE 'Used', HOURS 'Hrs',
-            T.DESCR 'Type', NOTE 'Comment', IF(APPROVE=1,'Yes','No') 'Approved?', 
-            AUDITID 'Last Mod.', REASON 'Reason' 
-        FROM REQUEST R, TIMETYPE T
-        WHERE ID='" . $_SESSION['userName'] .
-        "' AND R.TIMETYPEID=T.TIMETYPEID";
-echo $myq . "</br>"; //DEBUG
+switch($admin) {
+    case 0: //normal user, list only user's own reqs
 
-$result = $mysqli->query($myq);
-if (!$result) 
-throw new Exception("Database Error [{$mysqli->errno}] {$mysqli->error}");
+        $mysqli = connectToSQL();
+        $myq = "SELECT REFER 'Ref. No.', REQDATE 'Requested', USEDATE 'Used', HOURS 'Hrs',
+                    T.DESCR 'Type', NOTE 'Comment', IF(APPROVE=1,'Yes','No') 'Approved?', 
+                    AUDITID 'Last Mod.', REASON 'Reason' 
+                FROM REQUEST R, TIMETYPE T
+                WHERE ID='" . $_SESSION['userName'] .
+                "' AND R.TIMETYPEID=T.TIMETYPEID";
+        popUpMessage("$myq"); //DEBUG
 
-//build table
-resultTable($mysqli, $result);
+        $result = $mysqli->query($myq);
+        if (!$result) 
+        throw new Exception("Database Error [{$mysqli->errno}] {$mysqli->error}");
+
+        //build table
+        resultTable($mysqli, $result);
+        break;
+    
+    case 25: //supervisor, list division
+    case 50: //HR
+    case 99: //Sheriff
+    case 100: //full admin, complete raw dump
+        $mysqli = connectToSQL();
+        $myq = "SELECT *
+                FROM REQUEST";
+        //echo $myq . "</br>"; //DEBUG
+
+        $result = $mysqli->query($myq);
+        if (!$result) 
+        throw new Exception("Database Error [{$mysqli->errno}] {$mysqli->error}");
+
+        //build table
+        resultTable($mysqli, $result);
+        break;
+        
+}
 
 
 
