8c96f74cd8888a8183126330be2bc19627fa901e
danlumpp <danlumpp@ad210744-6c23-267b-c354-416c3078f2c7>
2012-06-13 13:41:57 +0000

    fixing errors

--- trunk/leave_sql.php
+++ trunk/leave_sql.php
@@ -12,12 +12,12 @@ $usedate = $mysqli->real_escape_string($_POST['usedate']);
 $hours = $mysqli->real_escape_string($_POST['hours']);
 $type = $mysqli->real_escape_string($_POST['type']);
 $comment = $mysqli->real_escape_string($_POST['comment']);
-$reqdate = $mysqli->real_escape_string(date("YY-MM-DD")); //current date in SQL date format YYYY-MM-DD
+$reqdate = $mysqli->real_escape_string(date("Y-m-d")); //current date in SQL date format YYYY-MM-DD
 
 //query to insert the record
 
 $myq="INSERT INTO REQUEST VALUES (ID, USEDATE, HOURS, TIMETYPEID, NOTE, APPROVE, REQDATE)
-        VALUES ('$ID', '$usedate', '$hours', '$type', '$comment', '0', $reqdate)";
+        VALUES ('$ID', '$usedate', '$hours', '$type', '$comment', '0', '$reqdate')";
 popUpMessage($myq); //DEBUG
 $result = $mysqli->query($myq);
 
