b5c387d316b9731df3dffb600c339fc58348a1f1
danlumpp@gmail.com <danlumpp@gmail.com@ad210744-6c23-267b-c354-416c3078f2c7>
2012-06-13 01:20:09 +0000

    Initial upload of leave form component and added table building function

---
+++ trunk/leave_sql.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * Insert a new record in REQUEST table from 
+ * user-entered form data
+ */
+
+
+$ID = strtoupper($_POST['ID']);
+$usedate = $_POST['usedate'];
+$hours = $_POST['hours'];
+$type = $_POST['type'];
+$comment = $_POST['comment'];
+$reqdate = date("YY-MM-DD"); //current date in SQL date format YYYY-MM-DD
+
+//query to insert the record
+$mysqli = connectToSQL();
+$myq="INSERT INTO REQUEST VALUES (ID, USEDATE, HOURS, TIMETYPEID, NOTE, APPROVE, REQDATE)
+        VALUES ('$ID', '$usedate', '$hours', '$type', '$comment', '0', $reqdate)";
+$myq = $mysqli->real_escape_string($myq);
+$result = $mysqli->query($myq);
+
+//show SQL error msg if query failed
+if (!$result) {
+throw new Exception("Database Error [{$mysqli->errno}] {$mysqli->error}");
+echo 'Request not accepted.';
+}
+else {
+    echo 'Request accepted. The reference number for this request is <b>' 
+        . $mysqli->insert_id . '</b>.';
+}
+    
+?>
