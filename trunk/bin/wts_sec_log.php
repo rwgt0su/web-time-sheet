18bf1bbe225c096bd9bdf1c2c85466e582907e73
awturner06@gmail.com <awturner06@gmail.com@ad210744-6c23-267b-c354-416c3078f2c7>
2012-07-20 14:41:34 +0000

    Secondary Logs in Menus

---
+++ trunk/bin/wts_sec_log.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function displaySecondaryLog($config){
+    echo '<h3>Secondary Logs</h3>';
+}
+?>
