8700b645e3516853564cb9abb21f5e0d72d3a2ea
awturner06 <awturner06@gmail.com>
2012-08-30 16:41:18 +0000

    Approve Report normal Dates Format

--- bin/time_request_functions.php
+++ bin/time_request_functions.php
@@ -846,7 +846,7 @@ function displayLeaveApproval(){
         if(strcmp($divisionID, "All") == 0){
             $myq = "SELECT DISTINCT REFER 'RefNo', RADIO 'Radio', CONCAT_WS(', ',LNAME,FNAME) 'Employee', 
                             DATE_FORMAT(REQDATE,'%d %b %Y %H%i') 'Requested', 
-                            DATE_FORMAT(USEDATE,'%a %d %b %Y') 'Used', DATE_FORMAT(BEGTIME,'%H%i') 'Start',
+                            DATE_FORMAT(USEDATE,'%b %d %Y') 'Used', DATE_FORMAT(BEGTIME,'%H%i') 'Start',
                             DATE_FORMAT(ENDTIME,'%H%i') 'End', HOURS 'Hrs',
                             T.DESCR 'Type', SUBTYPE 'Subtype', NOTE 'Comment', STATUS 'Status'                         
                         FROM REQUEST R, TIMETYPE T, EMPLOYEE E
@@ -858,7 +858,7 @@ function displayLeaveApproval(){
         else{
             $myq = "SELECT DISTINCT REFER 'RefNo', RADIO 'Radio', CONCAT_WS(', ',LNAME,FNAME) 'Employee', 
                             DATE_FORMAT(REQDATE,'%d %b %Y %H%i') 'Requested', 
-                            DATE_FORMAT(USEDATE,'%a %d %b %Y') 'Used', DATE_FORMAT(BEGTIME,'%H%i') 'Start',
+                            DATE_FORMAT(USEDATE,'%b %d %Y') 'Used', DATE_FORMAT(BEGTIME,'%H%i') 'Start',
                             DATE_FORMAT(ENDTIME,'%H%i') 'End', HOURS 'Hrs',
                             T.DESCR 'Type', SUBTYPE 'Subtype', NOTE 'Comment', STATUS 'Status'                         
                         FROM REQUEST R, TIMETYPE T, EMPLOYEE E
