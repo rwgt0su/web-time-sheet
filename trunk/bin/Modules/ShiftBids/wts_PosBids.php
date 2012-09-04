d416947e950191d1ceb8fd09d04133de7c399b2e
awturner06@gmail.com <awturner06@gmail.com@ad210744-6c23-267b-c354-416c3078f2c7>
2012-09-04 15:39:29 +0000

    Preparing for Shift Bids

---
+++ trunk/bin/Modules/ShiftBids/wts_PosBids.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of wts_PosBids
+ *
+ * @author awturner
+ */
+class wts_PosBids {
+    //put your code here
+}
+
+?>
---
+++ trunk/bin/Modules/ShiftBids/wts_positions.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Description of wts_positions
+ *
+ * @author awturner
+ */
+class wts_positions {
+    public $useDate, $startTime, $endTime, $available_nums, $divID, $descr, $reason, $auditID, $auditTime;
+    
+    public function wts_positions(){
+        
+        
+    }
+    public function setVariables(){
+        
+    }
+    public function addPosition(){
+        return true;
+    }
+    public function getPOSbyDate($date){
+
+    }
+    public function getPOSbyRef($refNo){
+
+    }
+}
+
+?>
