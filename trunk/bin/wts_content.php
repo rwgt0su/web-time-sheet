f00c87aac9d4df59e948289f361cd4917139ff00
awturner06@gmail.com <awturner06@gmail.com@ad210744-6c23-267b-c354-416c3078f2c7>
2013-01-06 19:41:23 +0000

    Greene Badges

--- trunk/bin/wts_content.php
+++ trunk/bin/wts_content.php
@@ -167,7 +167,7 @@ function displayContent($wts_content, $config){
 
 function displayWelcome($config){
     ?>
-    <div class="thumbnail"><img src="style/WellingtonBadge.gif" alt="" /></div>
+    <div class="thumbnail"><img src="style/SheriffBadgeGreene.gif" alt="" /></div>
     <h3><?php echo $config->getTitle(); ?></h3> 
     <p>Welcome to the Mahoning County Sheriff's Office Web Portal<br /><br />
         One you login you will have access to items such as Time Request Forms and Secondary Employment Logs</p>
@@ -175,7 +175,7 @@ function displayWelcome($config){
 }
 function displayAbout($config){
     ?>
-    <div class="thumbnail"><img src="style/WellingtonBadge.gif" alt="" /></div>
+    <div class="thumbnail"><img src="style/SheriffBadgeGreene.gif" alt="" /></div>
     <h3><?php echo $config->getTitle(); ?></h3> 
     <p>Welcome to the Mahoning County Sheriff's Office Web Portal</p><br />
     <p>This website is developed and maintained by the Mahoning County Sheriff's Office</p><br /><br /><br />
