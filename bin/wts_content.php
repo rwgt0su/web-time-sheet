57c87a7f84cc5451da11daff46b18848a0a9adb9
awturner06 <awturner06@gmail.com>
2013-04-17 03:05:26 +0000

    Print Friendly to Print Tables

--- bin/wts_content.php
+++ bin/wts_content.php
@@ -173,7 +173,7 @@ function displayContent($wts_content, $config){
             <?php
         }
         if(str_replace(dirname($_SERVER['REQUEST_URI'])."/", "", $_SERVER['PHP_SELF']) != "printFriendly.php" && $config->showPrinterFriendly){
-            echo '<a target="_blank" href="printFriendly.php?' . str_replace($_SERVER['PHP_SELF']."?", "", $_SERVER['REQUEST_URI']) . '">Printer Friendly</a>';
+            echo '<a target="_blank" href="printFriendly.php?' . str_replace($_SERVER['PHP_SELF']."?", "", $_SERVER['REQUEST_URI']) . '">Print Tables</a>';
         }
         myAlerts($config, $wts_content); 
     }
