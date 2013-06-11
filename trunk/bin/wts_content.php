f1461caed6297f8f1e6a8b36e6211675e48ad56b
awturner06@gmail.com <awturner06@gmail.com@ad210744-6c23-267b-c354-416c3078f2c7>
2013-06-11 18:15:42 +0000

    Bug Fix for Single page Time Request Forms (Debugging)

--- trunk/bin/wts_content.php
+++ trunk/bin/wts_content.php
@@ -176,6 +176,7 @@ function displayContent($wts_content, $config){
             $reqURI = dirname($_SERVER['REQUEST_URI'])."/";
         else
             $reqURI = dirname($_SERVER['REQUEST_URI']);
+        popupmessage(str_replace($reqURI, "", $_SERVER['PHP_SELF']));
         if(str_replace($reqURI, "", $_SERVER['PHP_SELF']) != "printFriendly.php" && $config->showPrinterFriendly){
             echo '<a target="_blank" href="printFriendly.php?' . str_replace($_SERVER['PHP_SELF']."?", "", $_SERVER['REQUEST_URI']) . '"> Print Tables</a>';
         }
